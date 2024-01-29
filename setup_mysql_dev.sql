9595b29ad3eed426bd780f1565b170a17305cc08
MKangabire <m.kangabire@alustudent.com>
2024-01-29 11:40:26 -0800

    console

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,10 @@
---creates a database--
+-- Create or use the existing database hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT  SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-GRANT ALL PRIVILEGES ON hbnb_test_db TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
