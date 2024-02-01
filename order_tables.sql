d1ced0939dde2c5b8a172d13acd2d01b7b2d80bc
nkubana0 <i.shema@alustudent.com>
2024-02-01 13:58:49 -0800

    commit

---
+++ order_tables.sql
@@ -0,0 +1,73 @@
+-- Create States Table
+CREATE TABLE states (
+    id VARCHAR(36) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+-- Create Amenities Table
+CREATE TABLE amenities (
+    id VARCHAR(36) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+-- Create Cities Table
+CREATE TABLE cities (
+    id VARCHAR(36) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    state_id VARCHAR(36) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
+
+-- Create Places Table
+CREATE TABLE places (
+    id VARCHAR(36) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    user_id VARCHAR(36) NOT NULL,
+    city_id VARCHAR(36) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (city_id) REFERENCES cities(id)
+);
+
+-- Create Place_Amenity Table
+CREATE TABLE place_amenity (
+    place_id VARCHAR(36) NOT NULL,
+    amenity_id VARCHAR(36) NOT NULL,
+    PRIMARY KEY (place_id, amenity_id),
+    FOREIGN KEY (place_id) REFERENCES places(id),
+    FOREIGN KEY (amenity_id) REFERENCES amenities(id)
+);
+
+-- Create Reviews Table
+CREATE TABLE reviews (
+    id VARCHAR(36) NOT NULL,
+    text TEXT NOT NULL,
+    user_id VARCHAR(36) NOT NULL,
+    place_id VARCHAR(36) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (place_id) REFERENCES places(id)
+);
+
+-- Create Users Table
+CREATE TABLE users (
+    id VARCHAR(36) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
