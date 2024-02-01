304f2ed1d40afbbbbeb6ec6f6bdbed52bab65cc6
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-02-02 01:54:05 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -16,46 +16,41 @@ class BaseModel:
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.created_at = kwargs.get('created_at', datetime.utcnow())
+        self.updated_at = kwargs.get('updated_at', datetime.utcnow())
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if hasattr(self, key):
+                if key in self.__dict__:
                     setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        cls = type(self).__name__
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+        """Updates updated_at with current time when the instance is changed"""
         from models import storage
         self.updated_at = datetime.utcnow()
         storage.new(self)
         storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        """Converts instance into a dictionary format"""
+        dictionary = self.__dict__.copy()
+        dictionary.update({'__class__': type(self).__name__})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        try:
-            del dictionary["_sa_instance_state"]
-        except KeyError:
-            pass
+        dictionary.pop("_sa_instance_state", None)
         return dictionary
 
     def delete(self):
         """
-        
+        Deletes the current instance from the storage
         """
         from models import storage
         storage.delete(self)
