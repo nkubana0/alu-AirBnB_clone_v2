69a98edb540a80163df10141ec3e17857f4aeef9
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-02-02 12:22:38 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -18,14 +18,14 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
         self.id = str(uuid.uuid4())
-        self.created_at = kwargs.get('created_at', datetime.utcnow())
-        self.updated_at = kwargs.get('updated_at', datetime.utcnow())
+        self.created_at = datetime.utcnow()  # Always set to current time
+        self.updated_at = datetime.utcnow()
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
+                if key in ("created_at", "updated_at"):
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key in self.__dict__:
-                    setattr(self, key, value)
+                    if key in self.__dict__:
+                        setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
