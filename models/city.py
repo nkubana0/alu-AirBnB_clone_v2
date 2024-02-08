5e7f13f7812e28f997ada89c7fe3c8401cf7ac37
nkubana0 <127296232+nkubana0@users.noreply.github.com>
2024-02-08 11:50:03 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy import Column, ForeignKey, String
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """
-    City inherits from BaseModel and Base (respect the order)
-    class attribute __tablename__ -
-        represents the table name, cities
-    class attribute name
-        represents a column containing a string (128 characters)
-        can’t be null
-    class attribute state_id
-        represents a column containing a string (60 characters)
-        can’t be null
-        is a foreign key to states.id
-    """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
+    """ The city class, contains state ID and name """
+
+    __tablename__ = 'cities'
+
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="cities")
