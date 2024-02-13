fc6c9df01874e1caf077eb6b356678b8c8d925b9
nkubana0 <i.shema@alustudent.com>
2024-02-13 10:21:11 +0000

    setup web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+if ! command -v nginx &> /dev/null
+then
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+echo "<html>
+<head>
+    <title>Test Page</title>
+</head>
+<body>
+    <h1>This is a test page for Nginx configuration</h1>
+</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+nginx_config="server {
+    listen 80;
+    server_name _;
+    location /hbnb_static {
+        alias /data/web_static/current/;
+        index index.html index.htm;
+    }
+}"
+
+sudo bash -c "echo \"$nginx_config\" > /etc/nginx/sites-available/default"
+
+sudo service nginx restart
